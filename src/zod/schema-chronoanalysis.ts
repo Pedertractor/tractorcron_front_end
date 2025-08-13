9086d9f0c9c6db503527e023810f5101d3a3bfdf
Pedro Henrique Prado <pedroh.prado@outlook.com>
2025-08-13 14:11:37 -0300

    refactor: change types schemas

--- src/zod/schema-chronoanalysis.ts
+++ src/zod/schema-chronoanalysis.ts
@@ -3,20 +3,19 @@ import { z } from 'zod';
 export const initialInformationsSchema = z.object({
   id: z.string().optional(),
   employeeId: z.number().optional(),
-  employeeName: z.string().optional(),
+  employeeName: z.string().min(4),
   employeeCardNumber: z
     .string()
     .min(4, 'Cartão obrigatório')
     .max(4, 'Precisamos apenas dos 4 dígitos')
     .regex(/^\d{4}$/, 'O cartão precisar ser em numeros!'),
   sectorId: z.number().optional(),
-  sectorName: z.string().optional(),
+  sectorName: z.string().min(4),
   sectorCostCenter: z
     .string()
     .min(4, 'Centro de custo obrigatório')
     .max(4, 'Precisamos apenas dos 4 dígitos')
-    .regex(/^\d{4}$/, 'O c.c precisar ser em numeros!')
-    .optional(),
+    .regex(/^\d{4}$/, 'O c.c precisar ser em numeros!'),
   employeeUnit: z.string().optional(),
   clientId: z.string().min(1, 'Cliente é obrigatório'),
   of: z.string().min(1, 'OF obrigatório'),
@@ -24,7 +23,7 @@ export const initialInformationsSchema = z.object({
   sop: z.string().min(1, 'SOP obrigatório'),
   revision: z.string().min(1, 'Revisão obrigatória'),
   internalCode: z.string().min(1, 'Código interno obrigatório'),
-  partNumber: z.string().min(1, 'Part Number obrigatório'),
+  partNumber: z.string().min(4, 'Part Number obrigatório'),
 });
 
 export type TypeInitialInformationsData = z.infer<
