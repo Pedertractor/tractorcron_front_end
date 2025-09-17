e3ed0a965c3c3009f666d34d8edf4afa6e9e2443
Pedro Henrique Prado <pedroh.prado@outlook.com>
2025-09-17 10:06:38 -0300

    feat: add internalCode in zod schema

--- src/zod/schema-chronoanalysis.ts
+++ src/zod/schema-chronoanalysis.ts
@@ -2,13 +2,6 @@ import { z } from 'zod';
 
 export const initialInformationsSchema = z.object({
   id: z.string().optional(),
-  employeeId: z.number().optional(),
-  employeeName: z.string().min(4),
-  employeeCardNumber: z
-    .string()
-    .min(4, 'Cartão obrigatório')
-    .max(4, 'Precisamos apenas dos 4 dígitos')
-    .regex(/^\d{4}$/, 'O cartão precisar ser em numeros!'),
   sectorId: z.number().optional(),
   sectorName: z.string().min(4),
   sectorCostCenter: z
@@ -16,7 +9,6 @@ export const initialInformationsSchema = z.object({
     .min(4, 'Centro de custo obrigatório')
     .max(4, 'Precisamos apenas dos 4 dígitos')
     .regex(/^\d{4}$/, 'O c.c precisar ser em numeros!'),
-  employeeUnit: z.string().optional(),
   clientId: z.string().min(1, 'Cliente é obrigatório'),
   of: z.string().min(1, 'OF obrigatório'),
   op: z.string().min(1, 'OP obrigatório'),
@@ -37,7 +29,7 @@ export type TypeInitialInformationsData = z.infer<
 
 export const filterChronoanalysis = z.object({
   partNumber: z.string().optional(),
-  of: z.string().optional(),
+  internalCode: z.string().optional(),
   unit: z.string().optional(),
   cardNumber: z.string().optional(),
   costCenter: z.string().optional(),
