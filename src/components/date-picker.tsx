7938e8497bd3dbd6ccf8f38db728f54f96ce5290
Pedro Henrique Prado <pedroh.prado@outlook.com>
2026-01-12 10:45:19 -0300

    feat: add filter year to dashboard home

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -5,7 +5,6 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
-import Label from './ui/label/label';
 import { CalendarRange } from './calendar/calendar';
 import { useState } from 'react';
 import type { DateRange } from 'react-day-picker';
@@ -16,33 +15,64 @@ type Props = {
 };
 export function DatePicker({ value, onChange }: Props) {
   const [open, setOpen] = useState(false);
+  const [isYear, setIsYear] = useState<string | null>(null);
+
+  const years = ['2025', '2026'];
+
+  function handleClickYear(year: string) {
+    if (year === isYear) {
+      onChange({
+        from: new Date(),
+        to: new Date(),
+      });
+      return setIsYear(null);
+    }
+
+    setIsYear(year);
+    onChange({
+      from: new Date(`01-01-${year}`),
+      to: new Date(`12-31-${year}`),
+    });
+  }
 
   return (
-    <div className=''>
-      <Label title='Data'>
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            <Button
-              variant='outline'
-              id='date'
-              className=' h-fit py-2 mt-0.5  rounded-lg text-secondary border-border'
-            >
-              <Calendar />
-
-              {value
-                ? `${value.from?.toLocaleDateString()} - ${value.to?.toLocaleDateString()}`
-                : 'Selecione um periodo'}
-              <ChevronDownIcon />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent
-            className='w-auto overflow-hidden p-0 bg-white border-none shadow-lg'
-            align='start'
+    <div className=' w-full flex items-center gap-2'>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant='outline'
+            size={'sm'}
+            className='text-secondary border-border cursor-pointer'
           >
-            <CalendarRange dateRange={value} setDateRange={onChange} />
-          </PopoverContent>
-        </Popover>
-      </Label>
+            <Calendar />
+            {value
+              ? `${value.from?.toLocaleDateString()} - ${value.to?.toLocaleDateString()}`
+              : 'Selecione um periodo'}
+            <ChevronDownIcon />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          className='w-auto overflow-hidden p-0 bg-white border-none shadow-lg'
+          align='start'
+        >
+          <CalendarRange dateRange={value} setDateRange={onChange} />
+        </PopoverContent>
+      </Popover>
+      {years.map((year, index) => (
+        <Button
+          variant={'outline'}
+          size={'sm'}
+          key={`${index}-${year}`}
+          className={`cursor-pointer text-secondary border-border ${
+            isYear === year
+              ? 'bg-background-blue-active border-background-base-blue-active'
+              : ''
+          }  `}
+          onClick={() => handleClickYear(year)}
+        >
+          {year}
+        </Button>
+      ))}
     </div>
   );
 }
