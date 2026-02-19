8bba871141b244d566f30c6465cf06f45e2492b7
pedro prado <pedro.prado@ptractor.com.br>
2026-02-19 10:59:39 -0300

    feat: add vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
-
+  server: {
+    host: '10.20.2.79',
+    port: 8888,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
